51eed5bd1e7fe9922af2449a01b52c0a48d51902
m4hs4n <m4hs4nnn@gmail.com>
2026-05-31 11:18:09 +0200

    Add classes project

---
+++ 02_Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace program
+{
+
+
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            System.Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Mahsan");
+            person.Introduce("stranger");
+        }
+    }
+}
