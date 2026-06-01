1339e9848f0a4ee982f104d0a304bb9cc6a7fcb1
m4hs4n <m4hs4nnn@gmail.com>
2026-06-01 11:06:51 +0200

    added access modifiers

---
+++ 06_access_modifiers/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Program
+{
+    public class Person
+    {
+        private DateTime _bithdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _bithdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _bithdate;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1998, 7, 9));
+            System.Console.WriteLine(person.GetBirthdate());
+            
+        }
+    }    
+        
+}
+
+
